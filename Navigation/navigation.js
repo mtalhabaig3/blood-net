3456f929c7efdc4885f1d408b37518d090302441
mtalhabaig3 <mtalhabaig3@gmail.com>
2022-08-12 23:36:11 +0500

    rendering requests through navigation

--- Navigation/navigation.js
+++ Navigation/navigation.js
@@ -1,49 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { onAuthStateChanged } from "firebase/auth";
-import Welcome from "../screens/Welcome";
-import { auth } from "../firebase";
+import Requests from "../Screens/Requests";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-  const [currUser, setCurrUser] = useState(null);
-  // const navigation = useNavigation();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setCurrUser(user);
-      } else {
-        setCurrUser(null);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
   return (
     <NavigationContainer>
-      {!currUser ? (
-        <Stack.Navigator
-          initialRouteName="Welcome"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Signup" component={Signup} />
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator initialRouteName="Requests">
-          <Stack.Screen
-            name="Requests"
-            component={List}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      )}
+      <Stack.Navigator
+        initialRouteName="Requests"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen
+          name="Requests"
+          component={Requests}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };

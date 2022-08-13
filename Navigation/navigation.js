b6c9fb107c4cb5148b6c63b8c6e00369ca4cc0ff
mtalhabaig3 <mtalhabaig3@gmail.com>
2022-08-13 15:56:03 +0500

    Configured navigation.js to handle logged in user

--- Navigation/navigation.js
+++ Navigation/navigation.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { onAuthStateChanged } from "firebase/auth";
 import Welcome from "../Screens/Welcome";
 import Signup from "../Screens/Signup";
 import Login from "../Screens/Login";
@@ -9,17 +10,42 @@ import Requests from "../Screens/Requests";
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
+  const [currUser, setCurrUser] = useState(null);
+  // const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrUser(user);
+      } else {
+        setCurrUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Requests" component={Requests} />
-      </Stack.Navigator>
+      {!currUser ? (
+        <Stack.Navigator
+          initialRouteName="Welcome"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="LoginScreen" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName="List">
+          <Stack.Screen
+            name="Requests"
+            component={Requests}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
